aa62afd015cfc39ba4bc49344f29a90f924ab315
garvit192 <130091842+garvit192@users.noreply.github.com>
2023-08-24 17:42:24 +0530

    added prelimeniary structure and styling to the site

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const Message = () => {
     return(
-        <div className="message">
+        <div className="message owner">
             <div className="messageInfo">
-                <span>John</span>
-                <p>Hey, how are you?</p>
+                <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p> hello</p>
+                <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1200" alt="" />
             </div>
         </div>
     )
